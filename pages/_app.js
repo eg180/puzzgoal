272fb59e9bd34c1c3390d4101dc96d19a4bb5895
Erick Gonzalez <ericksinbox@gmail.com>
2022-02-04 09:21:40 +0100

    fixed odd toast behavior

--- pages/_app.js
+++ pages/_app.js
@@ -7,17 +7,6 @@ import "react-toastify/dist/ReactToastify.css";
 function MyApp({ session, Component, ...pageProps }) {
 	return (
 		<>
-			<ToastContainer
-				position="top-right"
-				autoClose={5000}
-				hideProgressBar={false}
-				newestOnTop={false}
-				closeOnClick
-				rtl={false}
-				pauseOnFocusLoss
-				draggable
-				pauseOnHover
-			/>
 			<SessionProvider session={session}>
 				<Banner />
 				<Component {...pageProps} />
